b12de5fc806e7e28378f17abbf71f0696124ba56
Rahul Shah <rshahshah2890@gmail.com>
2017-09-18 21:49:21 +0530

    Boolean field in room

--- Timetable/admin.py
+++ Timetable/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-# from .models import TimeTable, Time, Room, Batch
-#
-# admin.site.register(TimeTable)
-# admin.site.register(Time)
-# admin.site.register(Room)
+from .models import Timetable, Time, Room
+
+admin.site.register(Timetable)
+admin.site.register(Time)
+admin.site.register(Room)
 # admin.site.register(Batch)
