9be57d530d94a82bcc4dcb3aab92cab46b755898
Rahul Shah <rshahshah2890@gmail.com>
2017-09-19 03:33:48 +0530

    Just a commit

--- Timetable/urls.py
+++ Timetable/urls.py
@@ -7,5 +7,5 @@ app_name = 'Timetable'
 urlpatterns = [
     url(r'enter/$', views.fill_timetable, name='fill_timetable'),
 
-    url(r'get_faculty/$', views.get_faculty, name='get_faculty'),
+    url(r'^get_faculty/$', views.get_faculty, name='get_faculty'),
 ]
