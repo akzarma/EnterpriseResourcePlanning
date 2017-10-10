b5b11ad5f3c5aa0c32551fe20686467d58c12458
akzarma <akzarma@gmail.com>
2017-10-10 14:16:04 +0000

    commit from pythonanywhere.com

--- EnterpriseResourcePlanning/settings.py
+++ EnterpriseResourcePlanning/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 # Custom User Model
 AUTH_USER_MODEL = 'UserModel.User'
 
-ALLOWED_HOSTS = ['192.168.0.110', '192.168.0.104', '192.168.43.169','127.0.0.1']
+ALLOWED_HOSTS = ['192.168.0.110', '192.168.0.104', 'akzarma.pythonanywhere.com', '192.168.43.169','127.0.0.1']
 
 # Application definition
 
--- Login/views.py
+++ Login/views.py
@@ -8,6 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 from General.models import StudentDivision
 from Registration.models import Faculty, Student
 from UserModel.models import User
+import json
 
 
 
@@ -30,6 +31,9 @@ def login_user(request):
 
 @csrf_exempt
 def login_android(request):
+    response = [{
+                    'userType': 'null'
+                    }]
     if request.method == 'POST':
         print(request.POST)
         try:
@@ -59,8 +63,8 @@ def login_android(request):
                     }]
                     return HttpResponse(str(student_response[0]))
             else:
-                return HttpResponse("{'userType': 'null'}")
+                return HttpResponse(str(response[0]))
         except:
-            return HttpResponse("Something is wrong")
-    print("inside android")
-    return HttpResponse("got")
+
+            return HttpResponse(str(response[0]))
+    return HttpResponse(str(response[0]))
