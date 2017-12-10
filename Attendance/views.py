eda4af3ffc78fd408aca667aaefc9d711f9be222
Shibashis <shibashismallik@gmail.com>
2017-12-10 17:00:14 +0530

    commit

--- Attendance/views.py
+++ Attendance/views.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 import datetime
@@ -6,17 +7,24 @@ import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from Registration.models import Student, Subject
+from General.models import FacultySubject, StudentDivision
+from Registration.models import Student, Subject, Faculty
 from .models import StudentAttendance, DailyAttendance
 
 
 # Create your views here.
 def index(request):
-    all_students = Student.objects.all()
-    all_subjects = Subject.objects.all()
+    user = request.user
+    selected_faculty_subject = request.POST.get('selected_faculty_subject')
+    selected_faculty_subject_obj = FacultySubject.objects.get(pk=selected_faculty_subject)
+    faculty_obj = Faculty.objects.get(user=user)
+    all_students = StudentDivision.objects.filter(division=selected_faculty_subject_obj.division)
+    all_subjects = [i.subject for i in FacultySubject.objects.filter(faculty=faculty_obj)]
     return render(request, "attendance.html", {
         'all_students': all_students,
-        'all_subjects': all_subjects
+        'all_subjects': all_subjects,
+        'selected_subject' : selected_faculty_subject_obj.subject.short_form,
+        'selected_division' : selected_faculty_subject_obj.division.division,
     })
 
 
@@ -26,7 +34,7 @@ def save(request):
         present = request.POST.getlist('present')
         subject = Subject.objects.get(pk=int(request.POST.get('subject')))
         division = request.POST.get('division')
-        all_students = StudentDetails.objects.all().values_list('pk', flat=True)
+        all_students = Student.objects.all().values_list('pk', flat=True)
         print(all_students)
         print(request.POST)
         print("present")
@@ -38,13 +46,13 @@ def save(request):
         whole = []
         whole_daily = []
         for student in present:
-            new = StudentAttendance(student=StudentDetails.objects.get(pk=student), subject=subject, division=division)
+            new = StudentAttendance(student=Student.objects.get(pk=student), subject=subject, division=division)
             whole.append(new)
             new.save()
             new_daily = DailyAttendance(attendance=new, date=datetime.datetime.today(), attended=True)
             whole_daily.append(new_daily)
         for student in absent:
-            new = StudentAttendance(student=StudentDetails.objects.get(pk=student), subject=subject, division=division)
+            new = StudentAttendance(student=Student.objects.get(pk=student), subject=subject, division=division)
             whole.append(new)
             new.save()
             new_daily = DailyAttendance(attendance=new, date=datetime.datetime.today(), attended=False)
