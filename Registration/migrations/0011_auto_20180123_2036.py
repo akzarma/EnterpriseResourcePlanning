46f04d05e1f82e57d86ac7eb89a47423bf5f410b
Rahul Shah <rshahshah2890@gmail.com>
2018-01-24 02:50:09 +0530

    Appname

--- Registration/migrations/0011_auto_20180123_2036.py
+++ Registration/migrations/0011_auto_20180123_2036.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('Registration', '0010_remove_faculty_dob'),
+        ('Registration', '0005_subject_semester'),
     ]
 
     operations = [
