f30c1c199de7feae08bf098f046d77fd21b4ecc9
Rahul Shah <rshahshah2890@gmail.com>
2018-02-16 02:26:33 +0530

    Merge branch 'dev' of http://github.com/akzarma/EnterpriseResourcePlanning into dev
    # Conflicts:
    #	db.sqlite3

--- Registration/urls.py
+++ Registration/urls.py
@@ -8,20 +8,20 @@ urlpatterns = [
     # /register/student - Register Student
     url(r'^student/$', views.register_student, name='register_student'),
     # /register/faculty - Register Faculty
-    # url(r'^faculty/$', views.register_faculty, name='register_faculty'),
+    url(r'^faculty/$', views.register_faculty, name='register_faculty'),
     # /register/subject - Register Subject
-    # url(r'^subject/$', views.register_subject, name='register_subject'),
+    url(r'^subject/$', views.register_subject, name='register_subject'),
     # /register/studentsuccess/ - Register Student
     url(r'^student/success/$', views.success_student, name='success_student'),
     #     # /register/facultysuccess/ - Register Faculty
-    # url(r'^faculty/success/$', views.success_faculty, name='success_faculty'),
+    url(r'^faculty/success/$', views.success_faculty, name='success_faculty'),
 
     # /register/view_subjects
-    # url(r'^view_subjects/$', views.view_subjects, name='view_subjects'),
+    url(r'^view_subjects/$', views.view_subjects, name='view_subjects'),
 
-    # url(r'^register_faculty_subject/$', views.register_faculty_subject, name='register_faculty_subject'),
+    url(r'^register_faculty_subject/$', views.register_faculty_subject, name='register_faculty_subject'),
     #     # /register/subjectsuccess/ - Register Subject
-    #     url(r'^subject/success/$', views.register_subject, name='register_success'),
+    url(r'^subject/success/$', views.register_subject, name='register_success'),
 
     # url(r'^success/$', views.success, name='register_success'),
     # /states/
