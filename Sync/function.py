37b6484791ccd7f79726f234a564900d7ddc4a85
akzarma <akzarma@gmail.com>
2018-02-23 19:57:31 +0530

    heroku installation

--- Sync/function.py
+++ Sync/function.py
@@ -1,14 +1,14 @@
-# import firebase_admin
-# from firebase_admin import credentials, db
-# import os
+import firebase_admin
+from firebase_admin import credentials, db
+import os
+
+# data = {'7':'eight'}
 #
-# # data = {'7':'eight'}
-# #
-# #
-# # root.child('new').set({"days":[{"day":"Monday","time":[{"facultyLecture":{"batch":"B3","room":"B303","subject":"DBMS"},"time":"0800-0900"}]}],"name":"MPK"})
-# # root.child('FE').child('ENTC').set({
-# #     '8':'eight'
-# # })
+#
+# root.child('new').set({"days":[{"day":"Monday","time":[{"facultyLecture":{"batch":"B3","room":"B303","subject":"DBMS"},"time":"0800-0900"}]}],"name":"MPK"})
+# root.child('FE').child('ENTC').set({
+#     '8':'eight'
+# })
 # cwd = os.getcwd()
 # app_name = '/EnterpriseResourcePlanning'
 # if cwd.__contains__(app_name):
@@ -18,8 +18,8 @@
 # app = firebase_admin.initialize_app(cred, {
 #     'databaseURL': 'https://viit-1cbaf.firebaseio.com/'
 # })
-#
-#
-# def write_to_firebase(json_string, next_child):
-#     root = db.reference()
-#     root.child(next_child).set(json_string)
+
+
+def write_to_firebase(json_string, next_child):
+    root = db.reference()
+    # root.child(next_child).set(json_string)
